3f4184ca9898986dcd44789d445a5f5366a1491b
Jorge Rivera <jariverai@gmail.com>
2019-09-20 10:46:28 -0500

    [Refactor] Sort imports

--- app/screens/Contacts.js
+++ app/screens/Contacts.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
 class Contacts extends Component {
   render() {
