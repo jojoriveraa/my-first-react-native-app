76015610c109945f081bf2e5a2953764f140c029
Jorge Rivera <jariverai@gmail.com>
2019-09-24 23:22:32 -0500

    [Feature] Render a <FlatList>

--- app/screens/Contacts.js
+++ app/screens/Contacts.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
+import * as DATA from '../config/data';
 
 class Contacts extends Component {
   render() {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>Contacts Screen</Text>
+        <FlatList
+          data={DATA.contacts}
+          renderItem={({item}) => <Text>{item.email}</Text>}
+          keyExtractor={item => item.email}
+        />
       </View>
     );
   }
