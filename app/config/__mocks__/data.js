9c9b1c37cd44fb9609ebb299d8f90957d5aff535
Jorge Rivera <jariverai@gmail.com>
2019-09-25 16:39:23 -0500

    [Test] Update var name

--- app/config/__mocks__/data.js
+++ app/config/__mocks__/data.js
@@ -1,40 +1,40 @@
-export const contact = [
-    {
-      gender: 'female',
-      name: {
-        title: 'miss',
-        first: 'marina',
-        last: 'hauser',
-      },
-      location: {
-        street: '6091 wiesenweg',
-        city: 'rügen',
-        state: 'rheinland-pfalz',
-        postcode: 35593,
-      },
-      email: 'marina.hauser@example.com',
-      login: {
-        username: 'bigpanda183',
-        password: 'herring',
-        salt: 'aAKjWewv',
-        md5: 'f81bca19827536560196e9f46de6b2c1',
-        sha1: '62c3c1ab42ef8ccc5f931109aeee303e521dbdca',
-        sha256:
-          '96e472bbf3b870cc640ca93f3371efd8870eb64c134ad1b941e9468fdc43e19d',
-      },
-      dob: '1987-05-23 22:45:12',
-      registered: '2012-02-01 17:41:54',
-      phone: '0386-6098000',
-      cell: '0172-7666141',
-      id: {
-        name: '',
-        value: null,
-      },
-      picture: {
-        large: 'https://randomuser.me/api/portraits/women/22.jpg',
-        medium: 'https://randomuser.me/api/portraits/med/women/22.jpg',
-        thumbnail: 'https://randomuser.me/api/portraits/thumb/women/22.jpg',
-      },
-      nat: 'DE',
+export const contacts = [
+  {
+    gender: 'female',
+    name: {
+      title: 'miss',
+      first: 'marina',
+      last: 'hauser',
     },
-  ];
+    location: {
+      street: '6091 wiesenweg',
+      city: 'rügen',
+      state: 'rheinland-pfalz',
+      postcode: 35593,
+    },
+    email: 'marina.hauser@example.com',
+    login: {
+      username: 'bigpanda183',
+      password: 'herring',
+      salt: 'aAKjWewv',
+      md5: 'f81bca19827536560196e9f46de6b2c1',
+      sha1: '62c3c1ab42ef8ccc5f931109aeee303e521dbdca',
+      sha256:
+        '96e472bbf3b870cc640ca93f3371efd8870eb64c134ad1b941e9468fdc43e19d',
+    },
+    dob: '1987-05-23 22:45:12',
+    registered: '2012-02-01 17:41:54',
+    phone: '0386-6098000',
+    cell: '0172-7666141',
+    id: {
+      name: '',
+      value: null,
+    },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/22.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/22.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/22.jpg',
+    },
+    nat: 'DE',
+  },
+];
