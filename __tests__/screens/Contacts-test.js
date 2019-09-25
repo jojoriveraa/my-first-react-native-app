c550c89a8402163ab71cdd94b2f93493c949734d
Jorge Rivera <jariverai@gmail.com>
2019-09-24 23:03:07 -0500

    [Test] FlatList should render correctly

--- __tests__/screens/Contacts-test.js
+++ __tests__/screens/Contacts-test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Text } from 'react-native';
+import {Text, FlatList} from 'react-native';
 import renderer from 'react-test-renderer';
 import Contacts from '../../app/screens/Contacts.js';
+import * as mocks from '../../app/config/__mocks__/data';
 
 describe('[Screen] Contacts', () => {
   const contactsRenderer = renderer.create(<Contacts />);
@@ -14,5 +15,12 @@ describe('[Screen] Contacts', () => {
   it('should contain `Contacts screen`', () => {
     const textComponentValue = contactsInstance.findByType(Text).props.children;
     expect(textComponentValue).toEqual('Contacts Screen');
+  });
+
+  describe('contains a <FlatList>', () => {
+    it('should render correctly', () => {
+      const flatListComponent = contactsInstance.findByType(FlatList);
+      expect(flatListComponent).not.toBeNull();
+    });
   });
 });
