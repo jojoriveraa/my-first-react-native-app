f3e88fb2b679ae2dffd868de6746348ef7dd443f
Jorge Rivera <jariverai@gmail.com>
2019-09-26 10:41:40 -0500

    [Test] Flat List should display all the contacts' email (#26)
    * [Test] Flat List should display all the contacts' email

--- __tests__/screens/Contacts-test.js
+++ __tests__/screens/Contacts-test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Text, FlatList} from 'react-native';
 import renderer from 'react-test-renderer';
-import Contacts from '../../app/screens/Contacts.js';
+import Contacts from '../../app/screens/Contacts';
 import * as mocks from '../../app/config/__mocks__/data';
 
 describe('[Screen] Contacts', () => {
@@ -12,16 +12,61 @@ describe('[Screen] Contacts', () => {
     expect(contactsRenderer).toMatchSnapshot();
   });
 
-  it('should contain `Contacts screen`', () => {
+  it('should contain `Contacts screen` title', () => {
     const textComponentValue = contactsInstance.findAllByType(Text)[0].props
       .children;
     expect(textComponentValue).toEqual('Contacts Screen');
   });
+  it('should contain a list of contacts', () => {
+    const flatListComponent = contactsInstance.findByType(FlatList);
+    expect(flatListComponent).not.toBeNull();
+  });
 
-  describe('contains a <FlatList>', () => {
-    it('should render correctly', () => {
-      const flatListComponent = contactsInstance.findByType(FlatList);
-      expect(flatListComponent).not.toBeNull();
+  describe('when the list of contacts is initialized', () => {
+    let contactsRenderer;
+    let instance;
+    let contactsList;
+
+    let spyDidMount = jest.spyOn(Contacts.prototype, 'componentDidMount');
+    let spyLoadData = jest
+      .spyOn(Contacts.prototype, 'loadData')
+      .mockImplementation(() => mocks.contacts);
+
+    beforeEach(() => {
+      contactsRenderer = renderer.create(<Contacts />);
+      instance = contactsRenderer.root;
+      contactsList = instance.findByType(FlatList);
+    });
+
+    afterEach(() => {
+      spyDidMount.mockClear();
+      spyLoadData.mockClear();
+    });
+
+    it('should invoke `componentDidMount`', () => {
+      expect(spyDidMount).toHaveBeenCalled();
+    });
+
+    it('should invoke `loadData`', () => {
+      expect(spyLoadData).toHaveBeenCalled();
+    });
+
+    describe('and contacts list is shown', () => {
+      let contactsEmails;
+
+      beforeEach(() => {
+        contactsEmails = contactsList.findAllByType(Text);
+      });
+
+      it('should display all the items', () => {
+        expect(contactsEmails.length).toEqual(mocks.contacts.length);
+      });
+
+      it('should display contacts` email', () => {
+        const expectedEmails = mocks.contacts.map(item => item.email);
+        const actualEmails = contactsEmails.map(item => item.props.children);
+        expect(actualEmails).toEqual(expect.arrayContaining(expectedEmails));
+      });
     });
   });
 });
